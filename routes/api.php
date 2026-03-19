97e3a927dd11b2dd28664adfa989192067f2b9a0
Louis <louis.x.michaud@gmail.com>
2026-03-19 13:32:19 -0400

    legin et enregistrement fini, dashboard debuter

--- routes/api.php
+++ routes/api.php
@@ -15,11 +15,10 @@ Route::apiResource('posts', PostController::class);
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
 Route::post('/logout', [AuthController::class, 'logout'])
-->middleware('auth:sanctum');
+    ->middleware('auth:sanctum');
 
 
 Route::middleware('auth:sanctum')->group(function () {
 
     Route::get('/users', [UserController::class, 'index']);
-
 });
